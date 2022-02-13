1a799c67fbb9b5f8bb19813f11b21a0f81458d1c
engin can <engin_can1528@hotmail.com>
2022-02-13 14:32:57 +0300

    .

--- src/app/layouts/main/main.module.ts
+++ src/app/layouts/main/main.module.ts
@@ -4,8 +4,9 @@ import { RouterModule } from '@angular/router';
 import { DashboardLayoutModule } from 'src/app/shared/layouts/dashboard-layout/dashboard-layout.module';
 import { MainRoutes } from './main.routing';
 import { MainComponent } from './main.component';
+import { MainHomeComponent } from './main-home/main-home.component';
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent,MainHomeComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(MainRoutes),
