31d3b2c6fd3117e8f03ae85d3bfc18b50a366d4b
engin can <engin_can1528@hotmail.com>
2022-02-13 14:42:22 +0300

    added top bar

--- src/app/shared/layouts/dashboard-layout/dashboard-layout.module.ts
+++ src/app/shared/layouts/dashboard-layout/dashboard-layout.module.ts
@@ -1,3 +1,4 @@
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +9,7 @@ import { RouterModule } from '@angular/router';
   declarations: [
     SideBarComponent,
     DashboardLayoutComponent,
+    TopBarComponent
   ],
   imports: [FormsModule, RouterModule, ],
   exports: [DashboardLayoutComponent, SideBarComponent],
